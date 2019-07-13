529dd4e659fcdb7d508bf693a720660434eaf4b3
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-13 02:54:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
-    
+
     if value_to_find == array[index]
       return index
     end
-    
+
   end
   return nil
 end
